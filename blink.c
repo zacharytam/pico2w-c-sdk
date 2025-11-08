8e6844a1dd7a4366c13564c507c90ddf83af89ce
zacharytam <78257053+zacharytam@users.noreply.github.com>
2025-11-09 05:27:02 +0800

    Update blink.c

--- blink.c
+++ blink.c
@@ -5,15 +5,13 @@
 #endif
 
 int main() {
-    // Initialize the onboard LED pin
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, true);
 
-    // Blink loop
     while (true) {
-        gpio_put(PICO_DEFAULT_LED_PIN, true);  // LED ON
+        gpio_put(PICO_DEFAULT_LED_PIN, true);
         sleep_ms(LED_DELAY_MS);
-        gpio_put(PICO_DEFAULT_LED_PIN, false); // LED OFF
+        gpio_put(PICO_DEFAULT_LED_PIN, false);
         sleep_ms(LED_DELAY_MS);
     }
 
