18b15d014cdd4e42144d71f06a566221eb0d64fb
zacharytam <78257053+zacharytam@users.noreply.github.com>
2025-11-09 04:38:44 +0800

    Update pico_sdk_import.cmake

--- pico_sdk_import.cmake
+++ pico_sdk_import.cmake
@@ -1,5 +1,4 @@
-# Import the Pico SDK from a relative path
-# (the workflow clones it into ${GITHUB_WORKSPACE}/pico-sdk)
+# Import Pico SDK from the path provided by workflow
+# This file tells CMake where to find the SDK
 set(PICO_SDK_PATH "${CMAKE_CURRENT_LIST_DIR}/pico-sdk")
-
 include(${PICO_SDK_PATH}/external/pico_sdk_import.cmake)
