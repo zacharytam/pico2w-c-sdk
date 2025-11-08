65859df5c9aef7ccee274277ccc9030436bb561e
zacharytam <78257053+zacharytam@users.noreply.github.com>
2025-11-09 02:11:08 +0800

    Update official_style_blink.c

--- src/official_style_blink.c
+++ src/official_style_blink.c
@@ -5,12 +5,21 @@
  */
 
 #include "pico/stdlib.h"
+#include "pico/binary_info.h"
+
+// Binary info like official examples
+bi_decl(bi_program_name("official_style_blink"));
+bi_decl(bi_program_description("Blink in official style"));
+bi_decl(bi_program_version_string("1.0"));
 
 #ifdef PICO_DEFAULT_LED_PIN
 #define LED_PIN PICO_DEFAULT_LED_PIN
 #else
 #define LED_PIN 25
 #endif
+
+// Binary info for LED
+bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
 
 int main() {
 #ifndef PICO_DEFAULT_LED_PIN
