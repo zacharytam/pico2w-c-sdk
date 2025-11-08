c74946ba71a67f62652ef0d4184fc0281c18f06c
zacharytam <78257053+zacharytam@users.noreply.github.com>
2025-11-09 04:47:39 +0800

    Update blink.c

--- blink.c
+++ blink.c
@@ -1,13 +1,5 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 #include "pico/stdlib.h"
 
-// Pico W devices use a GPIO on the WIFI chip for the LED,
-// so when building for Pico W, CYW43_WL_GPIO_LED_PIN will be defined
 #ifdef CYW43_WL_GPIO_LED_PIN
 #include "pico/cyw43_arch.h"
 #endif
@@ -16,38 +8,33 @@
 #define LED_DELAY_MS 250
 #endif
 
-// Perform initialisation
-int pico_led_init(void) {
-#if defined(PICO_DEFAULT_LED_PIN)
-    // A device like Pico that uses a GPIO for the LED will define PICO_DEFAULT_LED_PIN
-    // so we can use normal GPIO functionality to turn the led on and off
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-    return PICO_OK;
-#elif defined(CYW43_WL_GPIO_LED_PIN)
-    // For Pico W devices we need to initialise the driver etc
-    return cyw43_arch_init();
-#endif
-}
-
-// Turn the led on or off
-void pico_set_led(bool led_on) {
-#if defined(PICO_DEFAULT_LED_PIN)
-    // Just set the GPIO on or off
-    gpio_put(PICO_DEFAULT_LED_PIN, led_on);
-#elif defined(CYW43_WL_GPIO_LED_PIN)
-    // Ask the wifi "driver" to set the GPIO on or off
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
-#endif
-}
-
 int main() {
-    int rc = pico_led_init();
-    hard_assert(rc == PICO_OK);
+    int rc = 0;
+
+#if defined(PICO_DEFAULT_LED_PIN)
+    // Initialize GPIO for LED
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, true);
+#elif defined(CYW43_WL_GPIO_LED_PIN)
+    // Initialize Wi-Fi driver for Pico 2 W
+    rc = cyw43_arch_init();
+    if (rc != 0) return rc;
+#endif
+
+    // Blink loop
     while (true) {
-        pico_set_led(true);
+#if defined(PICO_DEFAULT_LED_PIN)
+        gpio_put(PICO_DEFAULT_LED_PIN, true);
         sleep_ms(LED_DELAY_MS);
-        pico_set_led(false);
+        gpio_put(PICO_DEFAULT_LED_PIN, false);
         sleep_ms(LED_DELAY_MS);
+#elif defined(CYW43_WL_GPIO_LED_PIN)
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
+        sleep_ms(LED_DELAY_MS);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
+        sleep_ms(LED_DELAY_MS);
+#endif
     }
+
+    return 0;
 }
