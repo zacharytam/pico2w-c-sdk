216157f80cdd591d103ba5209480b364b93aabc1
zacharytam <78257053+zacharytam@users.noreply.github.com>
2025-11-08 20:58:30 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -8,10 +8,23 @@ int main() {
     printf("=== Pico 2W LTE Router ===\n");
     printf("Build successful! 🎉\n");
     printf("Board: Pico W/2W\n");
+    printf("Basic firmware working\n");
     
+    // Blink LED if available (GP25 on Pico W)
+    #ifdef PICO_DEFAULT_LED_PIN
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    #endif
+    
+    int counter = 0;
     while (true) {
-        printf("Running...\n");
-        sleep_ms(5000);
+        printf("Counter: %d\n", counter++);
+        
+        #ifdef PICO_DEFAULT_LED_PIN
+        gpio_put(PICO_DEFAULT_LED_PIN, counter % 2);
+        #endif
+        
+        sleep_ms(1000);
     }
     
     return 0;
