81ec6d10b36f00481954200521e54137fde6682c
zacharytam <78257053+zacharytam@users.noreply.github.com>
2025-11-10 03:30:38 +0800

    Update lwipopts.h

--- lwipopts.h
+++ lwipopts.h
@@ -1,18 +1,23 @@
 #ifndef _LWIPOPTS_H
 #define _LWIPOPTS_H
 
+// Include the common minimal settings
 #include "lwipopts_examples_common.h"
 
+// Enable mDNS
+#define LWIP_MDNS_RESPONDER 1
+#define LWIP_IGMP 1
+#define LWIP_NUM_NETIF_CLIENT_DATA 1
+#define MDNS_RESP_USENETIF_EXTCALLBACK 1
+
+// HTTPD features
 #define LWIP_HTTPD_CGI 1
 #define LWIP_HTTPD_SSI 1
 #define LWIP_HTTPD_SSI_MULTIPART 1
 #define LWIP_HTTPD_SUPPORT_POST 1
-#define LWIP_MDNS_RESPONDER 1
-#define LWIP_IGMP 1
+#define LWIP_HTTPD_SSI_INCLUDE_TAG 0
 
-#define MEMP_NUM_SYS_TIMEOUT (LWIP_NUM_SYS_TIMEOUT_INTERNAL + 3)
-#define MEMP_NUM_TCP_PCB 12
-
+// Generated file containing HTML data
 #define HTTPD_FSDATA_FILE "pico_fsdata.inc"
 
 #endif
